941fc45d8e7413180436b00cd2665da6c998fb5e
dovbrand <dovibrand@gmail.com>
2022-05-06 15:22:08 -0400

    Add tokenlist CI

---
+++ actions/createList.js
@@ -0,0 +1,64 @@
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+function getAllFiles(dirPath, arrayOfFiles) {
+  let files = fs.readdirSync(dirPath)
+
+  arrayOfFiles = arrayOfFiles || []
+
+  files.forEach(function(file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+    } else if (path.extname(file) == '.json'){
+      arrayOfFiles.push(path.join(dirPath, "/", file))
+    }
+  })
+
+  return arrayOfFiles
+}
+
+const __dirname = dirname(fileURLToPath(
+    import.meta.url));
+const chainPath = path.join(__dirname, "../chains")
+
+
+const parseJSONFile = (path, chainId) => {
+    try {
+        const file = fs.readFileSync(path, 'utf8')
+        return JSON.parse(file)
+    } catch (e) {
+        console.error(`error parsing ${path}`)
+        console.error(e)
+        process.exit(1)
+    }
+}
+
+const currentTokenListPath = path.join(__dirname, '../tokenList.json')
+const currentList = parseJSONFile(currentTokenListPath)
+const files = getAllFiles(chainPath)
+let chainIdMap = new Map()
+files.forEach((fileName => {
+    const chainId = fileName.match('chains/[0-9]+')[0].split('/')[1]
+    if (chainIdMap.has(chainId)){
+      let arrayOfTokens = chainIdMap.get(chainId)
+      arrayOfTokens.push(parseJSONFile(fileName, chainId))
+      chainIdMap.set(chainId, arrayOfTokens)
+    }
+    else{
+      let arrayOfTokens = [parseJSONFile(fileName, chainId)]
+      chainIdMap.set(chainId, arrayOfTokens)
+    }
+
+}))
+const validateTokens = (tokens) => {
+    const existingAddress = new Set()
+    tokens.forEach((token) => {
+        if (existingAddress.has(token.address)) {
+            console.error(`${token.address} is repeated`)
+            process.exit(1)
+        }
+    })
+}
+
+fs.writeFileSync(currentTokenListPath, JSON.stringify({ tokens: Object.fromEntries(chainIdMap) }, null, 2))
