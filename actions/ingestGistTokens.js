b0c337f181c6867184325285a8ac5010ac280dd0
dfenstermaker <dannyfenstermaker@gmail.com>
2022-05-10 17:21:07 -0600

    add all tokens to the chains from gist

--- actions/ingestGistTokens.js
+++ actions/ingestGistTokens.js
@@ -1,4 +1,12 @@
 import axios from "axios";
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const CONTRACT_TOKEN_INFO_FILE = "contract-information.json";
+const MAINNET_CHAIN_ID = 43114;
+const FUJI_CHAIN_ID = 43113;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const MAINNET_TOKENLIST_URLS = [
   "https://raw.githubusercontent.com/pangolindex/tokenlists/main/top15.tokenlist.json",
@@ -12,11 +20,64 @@ export const FUJI_TOKENLIST_URLS = [
   "https://raw.githubusercontent.com/dasconnor/tokenlist/main/fuji.tokenlist.json",
 ];
 
-const values = Array.prototype.flat(
-  MAINNET_TOKENLIST_URLS.map(async (url) => {
-    const result = await axios.get(url);
-    console.log(result.data.tokens);
-  })
-);
+MAINNET_TOKENLIST_URLS.map(async (url) => {
+  const result = await axios.get(url);
+  const tokens = result.data.tokens;
+  tokens
+    .map((result) => {
+      const { logoURI, ...token } = result;
+      return {
+        ...token,
+        logoUrl: logoURI,
+        contractType: "ERC-20",
+        resourceLinks: [],
+      };
+    })
+    .forEach((token) => {
+      const tokenPath = path.resolve(
+        __dirname,
+        "../chains",
+        `${MAINNET_CHAIN_ID}`,
+        token.address
+      );
 
-console.log(values);
+      if (!fs.existsSync(tokenPath)) {
+        fs.mkdirSync(tokenPath);
+        fs.writeFileSync(
+          path.resolve(tokenPath, CONTRACT_TOKEN_INFO_FILE),
+          JSON.stringify(token, null, 2)
+        );
+      }
+    });
+});
+
+FUJI_TOKENLIST_URLS.map(async (url) => {
+  const result = await axios.get(url);
+  const tokens = result.data.tokens;
+  tokens
+    .map((result) => {
+      const { logoURI, ...token } = result;
+      return {
+        ...token,
+        logoUrl: logoURI,
+        contractType: "ERC-20",
+        resourceLinks: [],
+      };
+    })
+    .forEach((token) => {
+      const tokenPath = path.resolve(
+        __dirname,
+        "../chains",
+        `${FUJI_CHAIN_ID}`,
+        token.address
+      );
+
+      if (!fs.existsSync(tokenPath)) {
+        fs.mkdirSync(tokenPath);
+        fs.writeFileSync(
+          path.resolve(tokenPath, CONTRACT_TOKEN_INFO_FILE),
+          JSON.stringify(token, null, 2)
+        );
+      }
+    });
+});
