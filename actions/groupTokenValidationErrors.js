d21f797cbea3eb28d3862ef97c4ea7e307cff8f6
dfenstermaker <dannyfenstermaker@gmail.com>
2022-05-11 11:34:05 -0600

    quick change

--- actions/groupTokenValidationErrors.js
+++ actions/groupTokenValidationErrors.js
@@ -3,6 +3,8 @@ import {
   invalidTokensList,
   tokenValidationKeys,
 } from "./validateTokenStructure.mjs";
+import fs from "fs";
+import path from "path";
 
 console.log("number of invalid tokens: ", invalidTokensList.length);
 
@@ -14,3 +16,14 @@ console.log(
     return { ...acc, [key]: invalidTokensBuckets[key].length };
   }, {})
 );
+
+invalidTokensBuckets.logoUri.forEach((validation) => {
+  const { logoUrl, ...validToken } = validation.token;
+  const token = { ...validToken, logoUri: validation.token.logoUrl };
+  JSON.stringify(token, null, 2);
+  // console.log(validation.tokenContractInfoPath, token);
+  // validation.fs.writeFileSync(
+  //   path.resolve(tokenPath, CONTRACT_TOKEN_INFO_FILE),
+  //   JSON.stringify(token, null, 2)
+  // );
+});
