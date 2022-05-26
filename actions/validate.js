30efcba36bd03c7dcd08d94d3e6bdbde1450f1b6
dovbrand <dovibrand@gmail.com>
2022-05-26 15:36:39 -0400

    update validation

--- actions/validate.js
+++ actions/validate.js
@@ -11,7 +11,7 @@ import { getTokens } from "./getTokens.mjs";
 import chainInfoSchema from '../schema/chainInfoSchema.json' assert {type: 'json'};
 import contractInfoSchema from '../schema/contractInfoSchema.json'  assert {type: 'json'};
 
-let ajv = new Ajv({allErrors: true})
+let ajv = new Ajv({ allErrors: true })
 addFormats(ajv)
 
 const validateChainInfo = ajv.compile(chainInfoSchema)
@@ -24,16 +24,17 @@ const errors = fs.readdirSync(ROOT_PATH).reduce((acc, chainId) => {
     const chainInfo = JSON.parse(
         fs.readFileSync(path.resolve(ROOT_PATH, chainId, CHAIN_INFO_FILE), "utf8")
     );
-    
+
     validateChainInfo(chainInfo)
     let tokenErrors = getTokens(chainId, chainTokenIds).map(tokenInfo => {
-        validateContractInfo(tokenInfo)
-        return {[tokenInfo.address]: [validateContractInfo.errors]};
-    });
-    return { ...acc, [chainId]: {chainErrors: [...validateChainInfo.errors],  tokenErrors: tokenErrors } };
+        const valid = validateContractInfo(tokenInfo)
+        if (!valid) return { [tokenInfo.address]: [validateContractInfo.errors] }
+        else return
+    })
+    return { ...acc, [chainId]: { chainErrors: [...validateChainInfo.errors], tokenErrors: tokenErrors.filter(tokenError => {return (tokenError != null)})} };
 }, {});
 
-if(errors){
+if (errors) {
     core.setFailed(JSON.stringify(errors, null, 2))
 }
 
