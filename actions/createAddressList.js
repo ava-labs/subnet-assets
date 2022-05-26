41efb1699103534dd87ea2cf7a1e12e5eb523c99
dovbrand <dovibrand@gmail.com>
2022-05-26 11:01:52 -0400

    Update format

--- actions/createAddressList.js
+++ actions/createAddressList.js
@@ -8,14 +8,14 @@ import {
     ADDRESS_LIST_FILE,
 } from "./constants.mjs";
 
-const chains = fs.readdirSync(ROOT_PATH).map((chainId) => {
+const chains = fs.readdirSync(ROOT_PATH).reduce((acc, chainId) => {
     const chainTokenIds = path.resolve(ROOT_PATH, chainId);
     const tokens = fs
         .readdirSync(chainTokenIds)
         .filter(
             (pathValue) =>
                 ![CHAIN_INFO_FILE, CHAIN_LOGO_FILE, NATIVE_TOKEN_LOGO_FILE].includes(pathValue))
-    return {[chainId]: tokens}
-});
+    return { ...acc, [chainId]: [...tokens]}
+},{});
 
 fs.writeFileSync(ADDRESS_LIST_FILE, JSON.stringify(chains, null, 2))
