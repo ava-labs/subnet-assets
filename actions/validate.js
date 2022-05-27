81057cafdf287a316625cd91d1c8b1d96ca2976b
dovbrand <dovibrand@gmail.com>
2022-05-27 16:46:05 -0400

    refactor

--- actions/validate.js
+++ actions/validate.js
@@ -24,42 +24,14 @@ let errors = fs.readdirSync(ROOT_PATH).reduce((acc, chainId) => {
   validateChainInfo(chainInfo);
   let chainErrors = { ...validateChainInfo.errors };
 
-  if (chainInfo.chainId != Number(chainId))
-    chainErrors = {
-      ...chainErrors,
-      [Object.keys(chainErrors).length]: {
-        type: 'ChainId mismatch',
-        message: `ChainId ${chainInfo.chainId} in file does not match ${chainId}`,
-      },
-    };
+  chainErrors = checkChainId(chainInfo, chainId, chainErrors);
 
   let tokenErrors = getTokensWithAddresses(chainId, chainTokenIds).reduce((acc, { address, tokenInfo }) => {
     validateContractInfo(tokenInfo);
     let tokenError = { ...validateContractInfo.errors };
-    if (tokenInfo.address != address)
-      tokenError = {
-        ...tokenError,
-        [Object.keys(tokenError).length]: {
-          type: 'Address mismatch',
-          message: `${tokenInfo.address} in file does not match ${address}`,
-        },
-      };
-    if (isAddress(tokenInfo.address) && tokenInfo.address != getAddress(tokenInfo.address))
-      tokenError = {
-        ...tokenError,
-        [Object.keys(tokenError).length]: {
-          type: 'Bad Checksum',
-          message: `${tokenInfo.address} not properly checksummed`,
-        },
-      };
-    if (tokenInfo.chainId != Number(chainId))
-      tokenError = {
-        ...tokenError,
-        [Object.keys(tokenError).length]: {
-          type: 'ChainId mismatch',
-          message: `ChainId ${tokenInfo.chainId} in file does not match ${chainId}`,
-        },
-      };
+    tokenError = checkAddress(tokenInfo, address, tokenError);
+    tokenError = checkAddressChecksum(tokenInfo, tokenError);
+    tokenError = checkChainId(tokenInfo, chainId, tokenError);
     if (Object.keys(tokenError).length > 0) return { ...acc, [address]: tokenError };
     return { ...acc };
   }, {});
@@ -76,10 +48,45 @@ let errors = fs.readdirSync(ROOT_PATH).reduce((acc, chainId) => {
 errors = Object.entries(errors).filter(([chainId, errorType]) => {
   if (Object.keys(errorType.chainErrors).length == 0) delete errorType.chainErrors;
   if (Object.keys(errorType.tokenErrors) == 0) delete errorType.tokenErrors;
-
   return Object.keys(errorType).length != 0;
 });
 
 if (errors.length > 0) {
   core.setFailed(JSON.stringify(errors, null, 2));
 } else console.log('All checks passed');
+
+function checkAddressChecksum(tokenInfo, tokenError) {
+  if (isAddress(tokenInfo.address) && tokenInfo.address != getAddress(tokenInfo.address))
+    tokenError = {
+      ...tokenError,
+      [Object.keys(tokenError).length]: {
+        type: 'Bad Checksum',
+        message: `${tokenInfo.address} not properly checksummed`,
+      },
+    };
+  return tokenError;
+}
+
+function checkAddress(tokenInfo, address, tokenError) {
+  if (tokenInfo.address != address)
+    tokenError = {
+      ...tokenError,
+      [Object.keys(tokenError).length]: {
+        type: 'Address mismatch',
+        message: `${tokenInfo.address} in file does not match ${address}`,
+      },
+    };
+  return tokenError;
+}
+
+function checkChainId(info, chainId, errors) {
+  if (info.chainId != Number(chainId))
+    errors = {
+      ...errors,
+      [Object.keys(errors).length]: {
+        type: 'ChainId mismatch',
+        message: `ChainId ${info.chainId} in file does not match ${chainId}`,
+      },
+    };
+  return errors;
+}
