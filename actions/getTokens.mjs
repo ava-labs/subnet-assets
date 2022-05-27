29a119d484443068923fc8c0947234c49a4cd041
Connor Chevli <connorchev@gmail.com>
2022-05-27 16:27:50 -0400

    fix: prettify everything

--- actions/getTokens.mjs
+++ actions/getTokens.mjs
@@ -19,16 +19,16 @@ export function getTokens(chainId, chainTokenIds) {
     });
 }
 
-
 export function getTokensWithAddresses(chainId, chainTokenIds) {
   return fs
     .readdirSync(chainTokenIds)
     .filter((pathValue) => ![CHAIN_INFO_FILE, CHAIN_LOGO_FILE, NATIVE_TOKEN_LOGO_FILE].includes(pathValue))
     .map((tokenPath) => {
       return {
-        address: tokenPath, tokenInfo: JSON.parse(
+        address: tokenPath,
+        tokenInfo: JSON.parse(
           fs.readFileSync(path.resolve(ROOT_PATH, chainId.toString(), tokenPath, CONTRACT_TOKEN_INFO_FILE), 'utf8')
-        )
+        ),
       };
     });
 }
