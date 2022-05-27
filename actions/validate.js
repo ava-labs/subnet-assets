64b9d5617c327b0ac61f21370e9bdaa0d2f61f23
dovbrand <dovibrand@gmail.com>
2022-05-27 15:42:55 -0400

    update script to check chainId and address

--- actions/validate.js
+++ actions/validate.js
@@ -4,9 +4,10 @@ import * as core from '@actions/core';
 import addFormats from 'ajv-formats';
 import { ROOT_PATH, CHAIN_INFO_FILE } from './constants.mjs';
 import Ajv from 'ajv';
-import { getTokens } from './getTokens.mjs';
+import { getTokensWithAddresses } from './getTokens.mjs';
 import chainInfoSchema from '../schema/chainInfoSchema.json';
 import contractInfoSchema from '../schema/contractInfoSchema.json';
+import { getAddress, isAddress } from '@ethersproject/address';
 
 let ajv = new Ajv({ allErrors: true });
 addFormats(ajv);
@@ -21,31 +22,39 @@ let errors = fs.readdirSync(ROOT_PATH).reduce((acc, chainId) => {
   const chainInfo = JSON.parse(fs.readFileSync(path.resolve(ROOT_PATH, chainId, CHAIN_INFO_FILE), 'utf8'));
 
   validateChainInfo(chainInfo);
-  let tokenErrors = getTokens(chainId, chainTokenIds).map((tokenInfo) => {
-    const valid = validateContractInfo(tokenInfo);
-    if (!valid) return { [tokenInfo.address]: [validateContractInfo.errors] };
-    else return;
-  });
+
+  let chainIdError = {}
+  if (chainInfo.chainId != Number(chainId))
+    chainIdError = { [validateChainInfo.errors?.length || '0']: { type: 'ChainId mismatch', message: `ChainId ${chainInfo.chainId} in file does not match ${chainId}` } }
+
+  let tokenErrors = getTokensWithAddresses(chainId, chainTokenIds).reduce((acc, { address, tokenInfo }) => {
+    validateContractInfo(tokenInfo);
+    let tokenError = { ...validateContractInfo.errors }
+    if (tokenInfo.address != address) tokenError = { ...tokenError, [Object.keys(tokenError).length]: { type: 'Address mismatch', message: `${tokenInfo.address} in file does not match ${address}` } }
+    if (isAddress(tokenInfo.address) && tokenInfo.address != getAddress(tokenInfo.address)) {
+      tokenError = { ...tokenError, [Object.keys(tokenError).length]: { type: 'Bad Checksum', message: `${tokenInfo.address} not properly checksummed` } }
+    }
+    if (Object.keys(tokenError).length > 0) return { ...acc, [address]: tokenError };
+    return { ...acc };
+  }, {})
   return {
     ...acc,
     [chainId]: {
-      chainErrors: validateChainInfo.errors,
-      tokenErrors: tokenErrors.filter((tokenError) => {
-        return tokenError !== null;
-      }),
+      chainErrors: { ...validateChainInfo.errors, ...chainIdError },
+      tokenErrors: { ...tokenErrors }
     },
   };
 }, {});
 
-errors = Object.keys(errors).forEach(chainId => {
-  Object.keys(chainId).forEach(key => {
-    if(chainId[key] === null || chainId[key].length === 0){
-      delete chainId[key]
-    }
-  })
-  return delete errors[chainId]
+errors = Object.entries(errors).filter(([chainId, errorType]) => {
+  if (Object.keys(errorType.chainErrors).length == 0)
+    delete errorType.chainErrors
+  if (Object.keys(errorType.tokenErrors) == 0)
+    delete errorType.tokenErrors
+
+  return (Object.keys(errorType).length != 0)
 });
 
-if (errors){
-  core.setFailed(JSON.stringify(errors,null,2))
+if (errors) {
+  core.setFailed(JSON.stringify(errors, null, 2))
 }
