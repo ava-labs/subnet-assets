6149f275f17ff716350b1b517ff84393c23728c3
Cohan Carpentier <cohan.carpentier@gmail.com>
2022-06-06 14:18:52 -0400

    fix: split in separate file

--- actions/createChainsAndERC20List.js
+++ actions/createChainsAndERC20List.js
@@ -3,36 +3,21 @@ import path from 'path';
 import { ROOT_PATH, ERC20_TOKEN_LIST_FILE } from './constants.mjs';
 import { getTokens } from './getTokens.mjs';
 import { createChain } from './createChain.mjs';
-import { getAverageColor } from 'fast-average-color-node';
 import async from 'async';
+import { createTokens } from './createTokens.js';
 
 fs.readdir(ROOT_PATH, async (err, files) => {
-  if(!err && files) {
+  if (!err && files) {
     let chains = {};
 
     await async.forEachOf(files, async (chainId) => {
       // this is all chain paths. ie.../subnet-assets/chains/11111
       const chainTokenIds = path.resolve(ROOT_PATH, chainId);
       const tokens = getTokens(chainId, chainTokenIds);
-      const tokensWithAverageColor = await async.map(tokens, async (token) => {
-        if (token.logoUri) {
-          try {
-            const color = await getAverageColor(token.logoUri);
-            
-            return {
-              ...token,
-              tokenColor: color.hex,
-            };
-          } catch (err) {
-            console.log(err);
-          }
-        }
-        return token;
-      });
 
       const chain = {
         ...createChain(chainId, chainTokenIds),
-        tokens: tokensWithAverageColor.filter((token) => token.contractType === 'ERC-20'),
+        ...createTokens(tokens),
       };
 
       chains = {
---
+++ actions/createTokens.js
@@ -0,0 +1,22 @@
+import getColors from 'get-image-colors'
+
+export function createTokens(tokens) {
+  const tokensWithAverageColor = await async.map(tokens, async (token) => {
+    if (token.logoUri) {
+      try {
+        const color = await getColors(token.logoUri, {count: 1})
+        
+        return {
+          ...token,
+          tokenColor: color.map(color => color.hex())[0],
+        };
+      } catch (err) {
+        console.log('Error: ', err);
+      }
+    }
+    return token;
+  });
+
+  return tokensWithAverageColor.filter((token) => token.contractType === 'ERC-20'),
+
+}
