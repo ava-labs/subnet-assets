d8211927dc06fffe6ea2c847529c06247d777560
Mark Milner <mark.milner@avalabs.org>
2022-06-06 15:20:26 -0700

    Remove async; fix async loop

--- actions/createChainsAndERC20List.js
+++ actions/createChainsAndERC20List.js
@@ -3,14 +3,13 @@ import path from 'path';
 import { ROOT_PATH, ERC20_TOKEN_LIST_FILE } from './constants.mjs';
 import { getTokens } from './getTokens.mjs';
 import { createChain } from './createChain.mjs';
-import async from 'async';
 import { createTokens } from './createTokens.js';
 
 fs.readdir(ROOT_PATH, async (err, files) => {
   if (!err && files) {
     let chains = {};
 
-    await async.forEachOf(files, async (chainId) => {
+    await Promise.all(files.map(async (chainId) => {
       // this is all chain paths. ie.../subnet-assets/chains/11111
       const chainTokenIds = path.resolve(ROOT_PATH, chainId);
       const tokens = getTokens(chainId, chainTokenIds);
@@ -24,7 +23,7 @@ fs.readdir(ROOT_PATH, async (err, files) => {
         ...chains,
         [chain.chainId]: chain,
       };
-    });
+    }));
 
     // Writting the tokenList.erc20.json file
     fs.writeFileSync(ERC20_TOKEN_LIST_FILE, JSON.stringify(chains, null, 2));
--- actions/createTokens.js
+++ actions/createTokens.js
@@ -1,22 +1,23 @@
 import getColors from 'get-image-colors'
-import async from 'async';
 
 export async function createTokens(tokens) {
-  const tokensWithAverageColor = await async.map(tokens, async (token) => {
-    if (token.logoUri) {
-      try {
-        const color = await getColors(token.logoUri, {count: 1})
-        
-        return {
-          ...token,
-          tokenColor: color.map(color => color.hex())[0],
-        };
-      } catch (err) {
-        console.log('Error: ', err);
-      }
-    }
-    return token;
-  });
+  return Promise.all(
+    tokens
+      .filter((token) => token.contractType === 'ERC-20')
+      .map(async (token) => {
+        if (token.logoUri) {
+          try {
+            const color = await getColors(token.logoUri, { count: 1 })
 
-  return tokensWithAverageColor.filter((token) => token.contractType === 'ERC-20')
+            return {
+              ...token,
+              tokenColor: color.map(color => color.hex())[0],
+            };
+          } catch (err) {
+            console.log(err);
+          }
+        }
+        return token;
+      })
+  );
 }
