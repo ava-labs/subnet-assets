d5d8c01b0b4b77d12ebc5c03374513431969c8f1
Connor Chevli <connorchev@gmail.com>
2022-07-06 12:38:49 -0400

    fix: remove chainList, no longer needed

--- actions/constants.mjs
+++ actions/constants.mjs
@@ -10,7 +10,6 @@ export const NATIVE_TOKEN_LOGO_FILE = 'token-logo.png';
 export const TOKEN_LOGO_FILE = 'logo.png';
 export const CHAINS_FOLDER_URL = 'https://raw.githubusercontent.com/ava-labs/subnet-assets/main/chains';
 export const ERC20_TOKEN_LIST_FILE = path.resolve(__dirname, '../tokenList.erc20.json');
-export const CHAIN_LIST_FILE = path.resolve(__dirname, '../chainList.json');
 export const ADDRESS_LIST_FILE = path.resolve(__dirname, '../addressList.json');
 export const CONTRACT_TOKEN_INFO_FILE = 'contract-information.json';
 export const MAINNET_CHAIN_ID = 43114;
