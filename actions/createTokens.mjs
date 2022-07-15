ff8876d07a751b8e3fef46df932b5a4fd99bf95c
Connor Chevli <connorchev@gmail.com>
2022-07-15 06:41:13 -0400

    fix: token logo URIs no longer need to be manually added to the contract-information.json

--- actions/createTokens.mjs
+++ actions/createTokens.mjs
@@ -1,4 +1,6 @@
 import getColors from 'get-image-colors';
+import fs from 'fs';
+import { ROOT_PATH, TOKEN_LOGO_FILE } from './constants.mjs';
 
 export async function createTokens(tokens, contractTypeFilter) {
   return Promise.all(
@@ -8,9 +10,13 @@ export async function createTokens(tokens, contractTypeFilter) {
         if (token.logoUri) {
           try {
             const color = await getColors(token.logoUri, { count: 1 });
+            const hasLogoFile = fs
+              .readdirSync(`${ROOT_PATH}/${token.chainId}/${token.address}`)
+              .includes(TOKEN_LOGO_FILE);
 
             return {
               ...token,
+              logoUri: hasLogoFile ? `${ROOT_PATH}/${token.chainId}/${token.address}/${TOKEN_LOGO_FILE}` : undefined,
               tokenColor: color.map((color) => color.hex())[0],
             };
           } catch (err) {
