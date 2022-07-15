18c98ba0a0e493292f11f42a239e40c97a7f46d2
Connor Chevli <connorchev@gmail.com>
2022-07-15 16:38:04 -0400

    fix: set logo uri based on presence of png in contract directories

--- actions/constants.mjs
+++ actions/constants.mjs
@@ -5,18 +5,17 @@ import childProcess from 'child_process';
 export const __dirname = dirname(fileURLToPath(import.meta.url));
 export const __listsDirName = path.resolve(__dirname, '../_lists');
 
-// Pass a command line arg when testing locally to avoid fatals
-const gitRefFromCli = process.argv[2];
-const latestGitCommitHash =
-  gitRefFromCli ?? childProcess.execSync('git rev-parse --short "$GITHUB_SHA"').toString().trim();
+const isDev = process.argv[2] === 'dev';
+const gitRef = isDev ? 'main' : childProcess.execSync('git rev-parse --short "$GITHUB_SHA"').toString().trim();
 
-export const RAW_GITHUB_URL = `https://raw.githubusercontent.com/ava-labs/subnet-assets/${latestGitCommitHash}`;
+export const RAW_GITHUB_URL = `https://raw.githubusercontent.com/ava-labs/subnet-assets/${gitRef}`;
 export const ROOT_PATH = path.resolve('./chains');
 export const CHAIN_INFO_FILE = 'chain-information.json';
 export const CHAIN_LOGO_FILE = 'chain-logo.png';
 export const NATIVE_TOKEN_LOGO_FILE = 'token-logo.png';
 export const TOKEN_LOGO_FILE = 'logo.png';
 export const CHAINS_FOLDER_URL = `${RAW_GITHUB_URL}/chains`;
+export const getContractLogoUrl = (chainId, address) => `${CHAINS_FOLDER_URL}/${chainId}/${address}/${TOKEN_LOGO_FILE}`;
 export const ERC20_TOKEN_LIST_FILE = path.resolve(__listsDirName, 'tokenList.erc20.json');
 export const ADDRESS_LIST_FILE = path.resolve(__listsDirName, 'addressList.json');
 export const CONTRACT_TOKEN_INFO_FILE = 'contract-information.json';
--- actions/createTokens.mjs
+++ actions/createTokens.mjs
@@ -1,6 +1,7 @@
 import getColors from 'get-image-colors';
+import path from 'path';
 import fs from 'fs';
-import { ROOT_PATH, TOKEN_LOGO_FILE } from './constants.mjs';
+import { getContractLogoUrl, ROOT_PATH, TOKEN_LOGO_FILE, __dirname } from './constants.mjs';
 
 export async function createTokens(tokens, contractTypeFilter) {
   return Promise.all(
@@ -9,14 +10,15 @@ export async function createTokens(tokens, contractTypeFilter) {
       .map(async (token) => {
         if (token.logoUri) {
           try {
-            const color = await getColors(token.logoUri, { count: 1 });
             const hasLogoFile = fs
               .readdirSync(`${ROOT_PATH}/${token.chainId}/${token.address}`)
               .includes(TOKEN_LOGO_FILE);
+            const logoUri = hasLogoFile ? getContractLogoUrl(token.chainId, token.address) : undefined;
+            const color = await getColors(logoUri, { count: 1 });
 
             return {
               ...token,
-              logoUri: hasLogoFile ? `${ROOT_PATH}/${token.chainId}/${token.address}/${TOKEN_LOGO_FILE}` : undefined,
+              logoUri,
               tokenColor: color.map((color) => color.hex())[0],
             };
           } catch (err) {
