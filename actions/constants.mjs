9d77e08a8d360e9ac1359a3478edc2f454766f09
Connor Chevli <connorchev@gmail.com>
2022-07-25 16:53:34 -0400

    Update Chain and Network Token Logo Images to Use Git SHA instead of `main` (#79)
    * test out cli args in GH action CI
    
    * Commit from GitHub Actions (Build List)
    
    * use git sha in chains folder url constant
    
    * fix: pass gitref as a function arg
    
    * fix: build
    
    * Commit from GitHub Actions (Build List)
    
    * fix: no longer need to pass git sha as argument
    
    * Commit from GitHub Actions (Build List)
    
    * fix: don't use head, use git sha
    
    * Commit from GitHub Actions (Build List)
    
    * Remove console log
    
    * Commit from GitHub Actions (Build List)
    
    Co-authored-by: CI bot <connorch@users.noreply.github.com>

--- actions/constants.mjs
+++ actions/constants.mjs
@@ -1,15 +1,17 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import childProcess from 'child_process';
 
 export const __dirname = dirname(fileURLToPath(import.meta.url));
 export const __listsDirName = path.resolve(__dirname, '../_lists');
+export const latestGitCommitHash = childProcess.execSync('git rev-parse --short "$GITHUB_SHA"').toString().trim();
 
 export const ROOT_PATH = path.resolve('./chains');
 export const CHAIN_INFO_FILE = 'chain-information.json';
 export const CHAIN_LOGO_FILE = 'chain-logo.png';
 export const NATIVE_TOKEN_LOGO_FILE = 'token-logo.png';
 export const TOKEN_LOGO_FILE = 'logo.png';
-export const CHAINS_FOLDER_URL = 'https://raw.githubusercontent.com/ava-labs/subnet-assets/main/chains';
+export const CHAINS_FOLDER_URL = `https://raw.githubusercontent.com/ava-labs/subnet-assets/${latestGitCommitHash}/chains`;
 export const ERC20_TOKEN_LIST_FILE = path.resolve(__listsDirName, 'tokenList.erc20.json');
 export const ADDRESS_LIST_FILE = path.resolve(__listsDirName, 'addressList.json');
 export const CONTRACT_TOKEN_INFO_FILE = 'contract-information.json';
